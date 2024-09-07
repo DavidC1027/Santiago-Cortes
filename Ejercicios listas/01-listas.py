2ec3dbc85c6fd7303ef70e444cb03666906588d8
DavidC1027 <davids_cortesh@soy.sena.edu.co>
2024-09-06 23:01:22 -0500

    Add files via upload

---
+++ Ejercicios listas/01-listas.py
@@ -0,0 +1,7 @@
+#Escriba un programa Python para sumar todos los elementos de una lista.
+
+num =  [1, 2, 3, 4 ,5]
+suma = 0
+for i in num:
+    suma += i
+print(suma, end="")
---
+++ Ejercicios listas/03-listas.py
@@ -0,0 +1,5 @@
+#Escriba un programa Python para obtener el número más pequeño de una lista.
+
+num =  [12, 23, 34, 46 ,53]
+menor = min(num)
+print(f"El numero mas pequeño es: {menor}")
